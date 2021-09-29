2c27b649bc836eaa0c3d1c8cb32a9bb97709629d
koqiui <koqiui@163.com>
2021-09-29 22:29:13 +0800

    调整构建脚本

--- build.py
+++ build.py
@@ -7,8 +7,8 @@ import shutil
 import subprocess
 
 """
-    复制要发布的（依赖的 和 可执行)jar
-    用法：copy-dist-jars.py
+    构建 + 发布项目（到dist目录）
+    用法：build.py
 """
 
 # 当前脚本所在目录
@@ -19,34 +19,56 @@ distDir = os.path.join(__dir__, 'dist')
 distBinDir = os.path.join(distDir, 'bin')
 distJarsDir = os.path.join(distDir, 'jars')
 
+# 自定义设置提示
+customizeSettings = '调整好 dist/conf 下的 base.properties 和 app.properties， 即可运行 或 打包发分发了'
+
 
 def copyJars():
 
     os.chdir(__dir__)
 
-    # 1、复制 依赖jar
+    # 1、构建项目
+    mvnArgs = [
+        'mvn.cmd',
+        'clean',
+        'package',
+        'install'
+    ]
+    result = subprocess.call(mvnArgs)
+    if result == 0:
+        print('项目构建 成功')
+    else:
+        print('项目构建 失败')
+        sys.exit(result)
+
+    # 2、复制 依赖jar
     mvnArgs = [
         'mvn.cmd',
         'dependency:copy-dependencies',
         '-DoutputDirectory={0}'.format(distJarsDir)
     ]
-
     result = subprocess.call(mvnArgs)
-
     if result == 0:
         print('依赖的jar文件已复制到：{0}'.format(distJarsDir))
     else:
         print('依赖的jar文件复制失败')
         sys.exit(result)
 
-    # 2、复制 执行jar
+    # 3、复制 执行jar
     jarName = os.path.basename(__dir__) + '.jar'
     jarSrcFilePath = os.path.join(__dir__, 'target', jarName)
     if os.path.exists(jarSrcFilePath):
         shutil.copy(jarSrcFilePath, distBinDir)
-        print('{0} 已经复制到 {1} 下'.format(jarName, distBinDir))
+        print('{0} 已经复制到 {1}'.format(jarName, distBinDir))
 
-    sys.exit(result)
+    print()
+    print('------------------- :-) --------------------')
+    print()
+    print('恭喜您！dist 目录下就是项目运行所必需的全部文件')
+    print()
+    print(customizeSettings)
+
+    sys.exit()
 
 
 # 单独调用
--- copy-dist-jars.bat
+++
@@ -1,1 +0,0 @@
-@python ./copy-dist-jars.py
